8df62ff3005a6ca5a5697c0a17e823a484499df9
Anthony <anthonyvirayav@gmail.com>
2023-10-13 13:44:51 -0700

    new circle path

---
+++ src/main/kotlin/com/team2898/robot/commands/autos/CircleTest.kt
@@ -0,0 +1,15 @@
+package com.team2898.robot.commands.autos
+
+import com.pathplanner.lib.PathConstraints
+import com.pathplanner.lib.PathPlanner
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.CommandBase
+
+class CircleTest : CommandBase() {
+    private lateinit var autoCommandGroup: Command
+
+    override fun initialize() {
+        PathPlanner.loadPath("TestPath", PathConstraints(2.0, 2.0))
+//        PPSwerveControllerCommand("TestPath", pose)
+    }
+}
