6d8aa6e85b32624627a29247fe439a6e50c0e68e
Anthony <anthonyvirayav@gmail.com>
2023-12-09 14:09:35 -0800

    whar

--- src/main/kotlin/com/team2898/robot/commands/autos/TestAuto.kt
+++ src/main/kotlin/com/team2898/robot/commands/autos/TestAuto.kt
@@ -4,11 +4,21 @@ import com.pathplanner.lib.auto.AutoBuilder
 import com.pathplanner.lib.path.PathPlannerPath
 import com.team2898.robot.Constants
 import com.team2898.robot.subsystems.Drivetrain
+import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandBase
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 
 class TestAuto : CommandBase() {
+    private lateinit var autoCommandGroup: Command
     val path = PathPlannerPath.fromPathFile("Test Auto")
-    init {
-        AutoBuilder.followPathWithEvents(path)
+    override fun initialize() {
+        autoCommandGroup = SequentialCommandGroup (
+            AutoBuilder.followPathWithEvents(path)
+        )
+        autoCommandGroup.schedule()
+    }
+
+    override fun isFinished(): Boolean {
+        return false
     }
 }
