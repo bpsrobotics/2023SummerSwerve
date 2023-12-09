e809aa4f9a23cce0283fbc35fdb7687bc0a7e9ae
Anthony <anthonyvirayav@gmail.com>
2023-12-09 15:04:59 -0800

    whar

--- src/main/kotlin/com/team2898/robot/commands/autos/TestAuto.kt
+++ src/main/kotlin/com/team2898/robot/commands/autos/TestAuto.kt
@@ -1,15 +1,24 @@
 package com.team2898.robot.commands.autos
 
-import com.pathplanner.lib.PathConstraints
-import com.pathplanner.lib.PathPlanner
+import com.pathplanner.lib.auto.AutoBuilder
+import com.pathplanner.lib.path.PathPlannerPath
+import com.team2898.robot.Constants
+import com.team2898.robot.subsystems.Drivetrain
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandBase
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 
 class TestAuto : CommandBase() {
     private lateinit var autoCommandGroup: Command
-
+    val path = PathPlannerPath.fromPathFile("Test Auto")
     override fun initialize() {
-        PathPlanner.loadPath("TestPath", PathConstraints(2.0, 2.0))
-//        PPSwerveControllerCommand("TestPath", pose)
+        autoCommandGroup = SequentialCommandGroup (
+            AutoBuilder.followPathWithEvents(path)
+        )
+        autoCommandGroup.schedule()
+    }
+
+    override fun isFinished(): Boolean {
+        return false
     }
 }
