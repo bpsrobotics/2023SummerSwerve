fdea59909230bb8c530ab0cf9fd76cc18f097143
itl1 <lokanshu.ghorai@gmail.com>
2023-12-09 22:47:26 -0800

    we need more vars

--- src/main/kotlin/com/team2898/robot/subsystems/Shooter.kt
+++ src/main/kotlin/com/team2898/robot/subsystems/Shooter.kt
@@ -6,6 +6,7 @@ import com.revrobotics.CANSparkMaxLowLevel
 import com.team2898.robot.Constants.DriveConstants.kFeddderCanId
 import com.team2898.robot.Constants.DriveConstants.kFlywheelCanId
 import edu.wpi.first.math.controller.SimpleMotorFeedforward
+import edu.wpi.first.util.sendable.SendableBuilder
 import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
@@ -14,6 +15,10 @@ object Shooter : SubsystemBase() {
     private val flywheelController = CANSparkMax(kFlywheelCanId, CANSparkMaxLowLevel.MotorType.kBrushless)
     private val feederController = CANSparkMax(kFeddderCanId, CANSparkMaxLowLevel.MotorType.kBrushless)
 
+
+    //We need to define some stuff like RPM
+    // its not me copy pasting we really need this stuff
+    // so .. do i just put it in odometry or in constants
     val ready get() = (rawFlywheelSpeed - target.flywheel).value.absoluteValue < 10 &&
             (rawSpinnerSpeed - target.spinner).value.absoluteValue < 10 &&
             min(abs(rawFlywheelSpeed.value), abs(rawSpinnerSpeed.value)) > 10
@@ -62,5 +67,68 @@ object Shooter : SubsystemBase() {
         flywheelController.pidController.d  = 0.0
     }
 
+    fun spinUp(speeds: ShooterPowers = TargetAlignUtils.getPowers()) {
+        spunUpTime = Timer.getFPGATimestamp().seconds + 5.seconds
+        shooterPower = speeds
+        notMaxSpeed()
+    }
+
+    fun dumpSpinUp() {
+        spinUp(DUMP_SPEED)
+    }
+
+    fun stopShooter() {
+        spunUpTime = Timer.getFPGATimestamp().seconds
+        notMaxSpeed()
+    }
+
+    private var maxSpeed = false
+
+    fun maxSpeed() {
+        listOf(flywheelController, spinnerController).forEach {
+            it.setSmartCurrentLimit(40)
+        }
+        maxSpeed = true
+    }
+
+    fun notMaxSpeed() {
+        if (!maxSpeed) return
+        listOf(flywheelController, spinnerController).forEach {
+            it.setSmartCurrentLimit(20)
+        }
+        maxSpeed = false
+    }
+
+    override fun periodic() {
+        if (maxSpeed) {
+            flywheelController.set(1.0)
+            spinnerController.set(1.0)
+        } else if (Timer.getFPGATimestamp() < spunUpTime.value) {
+            var pid = spinnerPID.calculate(filteredSpinnerSpeed.average, target.spinner.value)
+            var ff = spinnerFF.calculate(target.spinner.value)
+            spinnerController.setVoltage((pid + ff) * 12)
+
+            pid = shooterPID.calculate(filteredShooterSpeed.average, target.flywheel.value)
+            ff = shooterFF.calculate(target.flywheel.value)
+            flywheelController.setVoltage((pid + ff) * 12)
+        } else {
+            flywheelController.set(0.0)
+            spinnerController.set(0.0)
+        }
+        filteredShooterSpeed.add(rawFlywheelSpeed.value)
+        filteredSpinnerSpeed.add(rawSpinnerSpeed.value)
+
+        lastShooterPos = flywheelController.encoder.position
+        lastSpinnerPos = spinnerController.encoder.position
+        timer.reset()
+    }
+
+    override fun initSendable(builder: SendableBuilder) {
+        builder.setSmartDashboardType("Subsystem")
+        builder.addDoubleProperty("shooter RPM", { rawFlywheelSpeed.value }) {}
+        builder.addDoubleProperty("spinner RPM", { rawSpinnerSpeed.value }) {}
+        builder.addDoubleProperty("shooter target", { target.flywheel.value }) {}
+        builder.addDoubleProperty("spinner target", { target.spinner.value }) {}
+    }
 
 }
